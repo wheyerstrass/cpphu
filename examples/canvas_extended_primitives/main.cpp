65976c118868819d4c2e4cdeef0d9a5c7c46701a
alex <>
2019-04-25 00:31:12 +0200

    started task 4

--- examples/canvas_extended_primitives/main.cpp
+++ examples/canvas_extended_primitives/main.cpp
@@ -17,21 +17,27 @@ int main(int argc, char* argv[])
 	line(img,{0,0},{64,64},color::green);
 	line(img,{64,0},{0,64},color::green);
 	
-	rectangle center_rect{{img.width()/2-16,img.height()/2-16},{32,32}};
+  rectangle center_rect {
+    {std::uint16_t(img.width()/2-16), std::uint16_t(img.height()/2-16)},
+      {32,32}
+  };
 	fill(img,center_rect,color::blue);
 	
-	point pos{main_window.width()/2-img.width()/2,main_window.height()/2-img.height()/2};
+  point pos {
+    std::uint16_t(main_window.width()/2-img.width()/2),
+      std::uint16_t(main_window.height()/2-img.height()/2)
+  };
 	while(main_window.handle_events() && !main_window.should_close())
 	{
 		if(main_window.is_pressed(key_id{'w'}))
-			pos.y-=1;
+			pos.y-=10;
 		if(main_window.is_pressed(key_id{'s'}))
-			pos.y+=1;
+			pos.y+=10;
 			
 		if(main_window.is_pressed(key_id{'a'}))
-			pos.x-=1;
+			pos.x-=10;
 		if(main_window.is_pressed(key_id{'d'}))
-			pos.x+=1;
+			pos.x+=10;
 			
 		if(main_window.is_pressed(key::escape))
 			break;
--- sgfx/include/sgfx/primitives.hpp
+++ sgfx/include/sgfx/primitives.hpp
@@ -4,21 +4,90 @@
 #include <sgfx/primitive_types.hpp>
 #include <sgfx/window.hpp>
 
+#include <cmath>
 #include <cstdint>
 
 namespace sgfx
 {
-	void plot(window& target, point p, color::rgb_color col);
-	
-	void clear(window& target, color::rgb_color col);
-	
-	void hline(window& target, point p, std::uint16_t length, color::rgb_color col);
-	void vline(window& target, point p, std::uint16_t length, color::rgb_color col);
-	
-	void fill(window& target, rectangle rect, color::rgb_color col);
-	
-	void line(window& target, point p0, point p1, color::rgb_color col);
-	
+  template<typename DrawTarget>
+    void plot(DrawTarget& target, point p, color::rgb_color col)
+    {
+      auto idx = p.toIdx(target.width(), target.height());
+      target.pixels()[idx] = col;
+    }
+
+  template<typename DrawTarget>
+    void clear(DrawTarget& target, color::rgb_color col)
+    {
+      const auto ww = target.width();
+      const auto wh = target.height();
+      for(auto i=0; i<ww*wh; i++)
+        target.pixels()[i] = col;
+    }
+
+  template<typename DrawTarget>
+    void hline(DrawTarget& target, point p, std::uint16_t length, color::rgb_color col)
+    {
+      const auto startPointX = p.x;
+      const auto startPointY = p.y;
+      length = fmin(length, target.width()-startPointX);
+
+      for (auto l=0; l<length; l++) {
+        point p { uint16_t(startPointX + l), startPointY };
+        const auto idx = p.toIdx(target.width(), target.height());
+        target.pixels()[idx] = col;
+      }
+    }
+
+  template<typename DrawTarget>
+    void vline(DrawTarget& target, point p, std::uint16_t length, color::rgb_color col)
+    {
+      const auto startPointX = p.x;
+      const auto startPointY = p.y;
+      length = fmin(length, target.height()-startPointY);
+
+      for (auto l=0; l<length; l++) {
+        point p { startPointX, uint16_t(startPointY + l) };
+        const auto idx = p.toIdx(target.width(), target.height());
+        target.pixels()[idx] = col;
+      }
+    }
+
+  template<typename DrawTarget>
+    void fill(DrawTarget& target, rectangle rect, color::rgb_color col)
+    {
+      const auto startPointX {rect.top_left.x};
+      const auto startPointY {rect.top_left.y};
+
+      for (auto y=0; y<rect.size.y; y++) {
+        for (auto x=0; x<rect.size.x; x++) {
+          point p { uint16_t(startPointX + x), uint16_t(startPointY + y) };
+          const auto idx = p.toIdx(target.width(), target.height());
+          target.pixels()[idx] = col;
+        }
+      }
+    }
+
+  template<typename DrawTarget>
+    void line(DrawTarget& target, point p0, point p1, color::rgb_color col)
+    {
+      const auto deltax { p1.x - p0.x }; // TODO vline
+      const auto deltay { p1.y - p0.y }; // TODO hline
+
+      const auto deltaySign { deltay >= 0 ? 1 : -1 };
+      const auto deltaerr { abs( double(deltay) / double(deltax) ) };
+      auto err { 0.0 };
+      auto y { p0.y };
+
+      for (auto x=p0.x; x<=p1.x; x++) {
+        plot(target, {x,y}, col);
+        err += deltaerr;
+        if (err >= 0.5) {
+          y += deltaySign;
+          err -= 1.0;
+        }
+      }
+    }
 }
 
 #endif
